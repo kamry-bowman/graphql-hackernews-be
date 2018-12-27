18619858481dcb22af3062ab4fab3133203ee171
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-26 21:13:42 -0700

    Implement link creation in Relay

--- hackernews/hackernews/schema.py
+++ hackernews/hackernews/schema.py
@@ -15,7 +15,12 @@ class Query(
     pass
 
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(
+    users.schema.Mutation,
+    links.schema.Mutation,
+    links.schema_relay.RelayMutation,
+    graphene.ObjectType
+):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
